974ce3bcfa94ce5b6caca3732637b2c6608b3abb
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-19 04:34:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,11 +23,11 @@ def shift_with_args(arr)
 end
 
 def using_concat(arr1,arr2)
-  arr.concat(arr2)
+  arr1.concat(arr2)
 end
 
-def using_inser(arr1,element)
-  arr.insert(4,element)
+def using_insert(arr1,element)
+  arr.insert(element,4)
 end
 
 def using_uniq(arr)
